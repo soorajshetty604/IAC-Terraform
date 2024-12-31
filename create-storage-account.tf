55fa8c2a41c18d1d6f3bee9e8a303b1473a23fad
Sooraj Shetty <Sooraj.Shetty@MediaKind.com>
2024-12-31 13:06:41 +0530

    Adding terraform config for creating storage account

---
+++ create-storage-account.tf
@@ -0,0 +1,29 @@
+#add required provider
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# create storage account in azure using terraform
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "testingstorageaccount"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+#add resource group
+resource "azurerm_resource_group" "resource_group" {
+  name     = "testingresourcegroup"
+  location = "East US"
+}
